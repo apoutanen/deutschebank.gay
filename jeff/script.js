1975b7c1502c1288ea8e9579a1cfce261d2a38f5
Jeff Stacey <jeffstacey111@gmail.com>
2021-05-25 23:50:39 -0700

    more on jeff's page

---
+++ jeff/script.js
@@ -0,0 +1,74 @@
+function log_in() {
+    // hide the login box
+    document.getElementById('login-box').classList.add('hidden');
+
+    // show welcome screen
+    document.getElementById('welcome-screen').classList.remove('hidden');
+
+    // set timeout for gif
+    window.setTimeout(gif_timeout, 2153);
+
+}
+
+function gif_timeout() {
+    console.log('hit')
+    document.getElementById('minecraft').src = 'images/minecraft_last.png';
+
+    document.getElementById('minecraft').classList.add('fadeout');
+}
+
+function hide_welcome_screen() {
+    document.getElementById('overlay').classList.add('hidden');
+    // picture time
+    document.getElementById('picture').classList.remove('hidden');
+}
+
+var spooky_intervals = []
+
+function enable_spooky_picturee() {
+    // enable spooky spnge
+    spooky_intervals.push(window.setInterval(toggle_invert, 1000));
+
+    b = document.getElementById('spooky-button')
+    if (b.value == "Spooky Mode")
+    {
+        b.value = "More Spooky"
+    } else if (b.value == "More Spooky") {
+        b.value = "Even More Spooky"
+    }
+}
+
+function disable_spooky_mode() {
+    for (i = spooky_intervals.length - 1; i >= 0; i--) {
+        window.clearInterval(spooky_intervals[i]);
+    }
+    spooky_intervals = []
+    document.getElementById('picture').classList.remove('inverted');
+}
+
+// flashes spooky picturee bob
+function toggle_invert() {
+    document.getElementById('picture').classList.toggle('inverted');
+}
+
+pic_idx = 0;
+available_pics = [
+    'images/spongebob.jpg',
+    'images/horse.jpg',
+    'images/basketball.png',
+    'images/tape_measure.jpg',
+    'images/boss.jpg',
+    'images/morgan_freeman.png',
+    'images/crappie.jpg',
+    'images/stars.png',
+    'images/evil_queen.png',
+    'images/bean.jpg',
+    'images/garfield.jpg',
+    'images/larry.jpg',
+    'images/toilet.jpg',
+    ];
+
+function next_picture() {
+    pic_idx = (pic_idx + 1) % available_pics.length;
+    document.getElementById('pic').src = available_pics[pic_idx]
+}
